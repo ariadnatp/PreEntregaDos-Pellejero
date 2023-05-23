34e9ba7c5fe30053b6e67a07e07ef1f5502a3011
Ariadna Pellejero <pellejeroariadna@gmail.com>
2023-05-22 22:15:20 -0300

    preEntregaUno

--- js.js
+++ js.js
@@ -1,24 +1,24 @@
 
 const pasajeKg = 1;
 const pasajeGr = 1000;
+let pasaje;
+let nombreUsuario = prompt("¡Hola! Ingresa tu nombre: ");
+alert("Te damos la bienvenida a la calculadora de pasajes kg/gr, " + nombreUsuario + "!");
 
-let nombreUsuario = prompt("hola! ingresa tu nombre: ")
-alert ("te damos la bienvenida a la calculadora de pasajes kg/gr "+nombreUsuario);
+do {
+   let pasaje = prompt("¿Qué pasaje necesitas hacer?\n- a KG\n- a GR\n- SALIR"); 
 
-function calcular(){
-    if(pasaje == "kg" || pasaje == "KG"){
-        let numeroGr = parseInt(prompt("ingrese el número que quieres pasar a Kg:"));
-        let multiplicacionGrAKg = (numeroGr * pasajeKg);
-        let resultadoKg = (multiplicacionGrAKg / pasajeGr);
+    if (pasaje == "kg" || pasaje == "KG") {
+        let numeroGr = parseInt(prompt("Ingresa el número que quieres pasar a Kg:"));
+        let multiplicacionGrAKg = numeroGr * pasajeKg;
+        let resultadoKg = multiplicacionGrAKg / pasajeGr;
         alert("Gr (el número que ingresaste) --> " + numeroGr + "\nKg (resultado) --> " + resultadoKg);
-    }else if (pasaje == "gr" || pasaje == "GR"){
-        let numeroKg = parseInt(prompt("ingrese el número que quieres pasar a Gr:"));
-        let multiplicacionKgAGr = (numeroKg * pasajeGr);
-        let resultadoGr = (multiplicacionKgAGr / pasajeKg);
+    } else if (pasaje == "gr" || pasaje == "GR") {
+        let numeroKg = parseInt(prompt("Ingresa el número que quieres pasar a Gr:"));
+        let multiplicacionKgAGr = numeroKg * pasajeGr;
+        let resultadoGr = multiplicacionKgAGr / pasajeKg;
         alert("Kg (el número que ingresaste) --> " + numeroKg + "\nGr (resultado) --> " + resultadoGr);
+    } else{
+        alert("hasta luego " +nombreUsuario);
     }
-}
-do{
-    let pasaje = prompt ("que pasaje necesitas hacer? \n- a KG \n- a GR \n- SALIR");
-    calcular();
-}while (pasaje == "salir" || pasaje == "SALIR");
+} while (pasaje !== "salir" || pasaje !== "SALIR");
